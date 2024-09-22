600c1b531ff88694559022a81e28b0ee495accec
ahmadalanib <alnaib888@gmail.com>
2024-09-22 11:49:03 +0200

    add valid

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from 'express';
+import { RegosterValidation } from '../validation/register.validation';
 
 export const Register=(req:Request,res:Response)=>{
-    res.send('Register');
+  const body=req.body;
+
+  const {error}=RegosterValidation.validate(body);
+
+  if(error){
+    return res.status(400).send(error.details)
+  }
+
+  if(body.password !== body.confirm_password){
+    return res.status(400).send('Password and confirm password do not match');
+  }
+    res.send(body);
 }
